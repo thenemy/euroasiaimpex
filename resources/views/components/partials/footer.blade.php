b9a22520b469ba0b6f45c30f15e464a3549cf626
Olexander <abbos.t.2001@gmail.com>
2021-12-24 23:13:10 +0500

    123

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 column">
                     <div class="footer-widget about-widget">
                         <figure class="footer-logo"><a href="index.blade.php"><img src="
-{{$logo->logo_220_80}}"
+                                {{$logo->logo_220_80}}"
                                                                                     alt="">
                             </a>
                         </figure>
@@ -44,17 +44,19 @@
                             <h3>{{$footer->follows_us}}</h3>
                             <div class="links">
                                 @foreach($footer->social_links as $social_link)
-                                    <a href="{{$social_link->link}}"><span
+                                     <a href="{{$social_link->link}}"><span
                                             class="fa fa-{{$social_link->name_of_icon}}"></span></a>
                                 @endforeach
                             </div>
-
                         </div>
 
                     </div>
+
                 </div>
 
-
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 d-flex align-items-end" style="margin-left: auto;">
+                    <img class="image right" src="{{asset('images/logo-turkish.png')}}" alt="" width="200px" height="76px" style=" float: right;.">
+                </div>
             </div>
 
         </div>
